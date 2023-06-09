a363a8e0744003ae715d314c3c14c484edf09acf
Jhonatan <larico1599@gmail.com>
2023-06-08 21:42:51 -0500

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,6 +13,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StoreContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
 
 
 var app = builder.Build();
@@ -27,5 +29,21 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+//Create the database and run the migration at runtime
+/*This code allows to get access to a Scope service inside our program class where we do not have the ability to inject a servce*/
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch(Exception ex)
+{
+    logger.LogError(ex, "An error ocurred during migration");
+}
 
 app.Run();
